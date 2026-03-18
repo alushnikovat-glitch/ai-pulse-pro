cb66d49949fdc587784d8951ca8591641e5c37b7
alushnikovat-glitch <alushnikovat@gmail.com>
2026-03-18 18:05:31 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-{
-  "buildCommand": "vite build",
-  "outputDirectory": "dist",
-  "framework": "vite",
-  "rewrites": [
-    { "source": "/api/(.*)", "destination": "/api/$1" },
-    { "source": "/(.*)", "destination": "/index.html" }
-  ]
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
