c9af1bd35b966824bbaf3eb0bc5e4b73689e446c
alushnikovat-glitch <alushnikovat@gmail.com>
2026-03-24 23:53:22 +0700

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -36,6 +36,7 @@ export default async function handler(req, res) {
         email,
         offerId,
         currency: "RUB",
+        buyerLanguage: "ru",
         successUrl: "https://ai-pulse-pro.vercel.app?paid=1",
         failUrl: "https://ai-pulse-pro.vercel.app?paid=0",
       };
